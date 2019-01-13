a90211c1ea7c24e38e0264aa84963fdba26c82a4
Salman Ahmad <salman.neim@gmail.com>
2019-01-13 00:35:57 +0000

    Done.

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -11,3 +11,9 @@ describe "#introduction_with_language" do
     expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
   end
 end
+
+def #introduction_with_language(name)
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
+end 
+
+  
