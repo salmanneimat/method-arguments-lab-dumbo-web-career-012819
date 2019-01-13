df6ae4c2fbceb975311e342eab77237d9915d0eb
Salman Ahmad <salman.neim@gmail.com>
2019-01-13 00:31:30 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -11,3 +11,9 @@ describe "#introduction_with_language" do
     expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
   end
 end
+
+def introduction(name)
+  puts "Hi, my name is # {name}."
+end 
+
+  
